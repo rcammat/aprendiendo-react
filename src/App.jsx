7f40b8b33fe04049a362d457c0e6763a924c8294
rcammat <mateosraimundo@gmail.com>
2023-04-09 01:36:39 +0200

    Firts Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
+export function App () {
+    return (
+        <section className="App">
+            <TwitterFollowCard userName="beakill_" name="Beakill" />
+            <TwitterFollowCard userName="pikachiina" name="Pikachina Edition" />
+        </section>
+    )
+}
---
+++ src/TwitterFollowCard.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+export function TwitterFollowCard ({userName, name, isFollowing}){
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+          <img
+            className="tw-followCard-avatar" 
+            alt="Avatar" 
+            src={`https://unavatar.io/${userName}`}
+          />
+          <div className="tw-followCard-info">
+            <strong>{name}</strong>
+            <span className='tw-followCard-infoUserName'>@{userName}</span>
+          </div>
+      </header>
+      <aside>
+        <button className="tw-followCard-button">
+            Seguir
+        </button>
+      </aside>
+    </article>
+  )
+}
